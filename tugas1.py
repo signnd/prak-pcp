727d0444587259e9ca9e0fcc594e0406fcce57d4
signnd <nfffqdb@gmail.com>
2022-05-28 10:32:36 +0800

    inv

--- tugas1.py
+++ tugas1.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 plat = cv2.imread('plat-no.jpeg', cv2.IMREAD_GRAYSCALE)
-th, plat_th = cv2.threshold(plat, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+th, plat_th = cv2.threshold(plat, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
 
 plat_fld = plat_th.copy()
 h, w = plat_th.shape[:2]
@@ -16,7 +16,7 @@ cv2.imshow('Gambar Asli', plat)
 cv2.imshow('Gambar Biner', plat_th)
 cv2.imshow('Gambar Floodfill', plat_fld)
 cv2.imshow('Gambar Invert', plat_fld_inv)
-# cv2.imshow('Foreground', plat_fore)
+cv2.imshow('Foreground', plat_fore)
 cv2.waitKey(0)
 
 r = cv2.selectROI("gambar", plat_fld_inv)
